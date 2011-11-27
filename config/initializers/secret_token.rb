7c6fb46d00e0fe2e872fd37dbe25af30c9d39839
Takanashi Ginpei <ginpei@gmail.com>
2011-11-28 01:42:27 +0900

    最初

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memobook::Application.config.secret_token = '5531366b7801122a00da76b966585d24c5b490df0548d2adc7a73be230b19d39fd6a48c0ab90d95c94c4dace9fefea33051dea71138bc40ea77c6e844e884e95'
